b24484d5980773c9214ceed0d91214c7011b6e69
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2016-07-10 17:21:32 -0300

    create and update fix

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -31,15 +31,18 @@ class TravelsController < ApplicationController
   def create
     @user_id = 1 #current_user.id
     @travel = Travel.new(travel_params)
+    @signed_travels = User.find(@user_id).travels.pluck(:id)
     @travel.save
-    render json: @travel
+    render json: TravelShowPresenter.new(@travel,@signed_travels).to_json
   end
 
   def update
+    @user_id = 1 #current_user.id
     @travel = Travel.find(params[:id])
     @travel.update_attributes(travel_params)
+    @signed_travels = User.find(@user_id).travels.pluck(:id)
     @travel.save
-    render json: @travel
+    render json: TravelShowPresenter.new(@travel,@signed_travels).to_json
   end
 
   def destroy
@@ -50,6 +53,7 @@ private
 
   def travel_params
       params.require(:travel).permit(
+      :car_id,
       :departure,
       :description,
       :origin,
