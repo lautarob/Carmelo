49cdc3603c8cb30c5a337c48e8353bba08842e99
Jorge de los Santos <tebayoso@gmail.com>
2016-07-10 17:19:29 +0200

    Fixed typo in association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :conversations
-  has_many :car
+  has_many :cars
   has_and_belongs_to_many :travels
 
   # Include default devise modules. Others available are:
