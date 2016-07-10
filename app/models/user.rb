6c66fa2a069bc539a88694e31a551ecd937f78cb
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2016-07-10 00:04:07 -0300

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_and_belongs_to_many :conversations
   has_many :cars
-  
-  # Include default devise modules.
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+  
 end
