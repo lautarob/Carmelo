e08c03dac7417cb797f4792b48b8dc8ac5332ad9
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2016-07-10 04:17:16 -0300

    Travel Controller fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  
   resources :users
   resources  :travels, :except => [:new,:edit] do
     collection do
