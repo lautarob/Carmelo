73357840030db34f1d49fe64a433780a5e52db7f
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2016-07-10 21:28:25 -0300

    facebook login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,17 +2,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    debugger
     @user = User.from_omniauth(request.env["omniauth.auth"])
     @token = request.env["omniauth.auth"].credentials.token
     @graph = Koala::Facebook::API.new(@token)
 
     if @user.persisted?
-      debugger
       sign_in @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      debugger
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
     :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    debugger
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
