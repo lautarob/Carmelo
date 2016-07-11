0913b76e5dd03ee26e067ae4324af0844f47bde4
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2016-07-10 21:26:45 -0300

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
+    debugger
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
