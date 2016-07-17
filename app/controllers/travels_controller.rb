5b64f94236e619fc869e0aed226a74014869dd67
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2016-07-17 15:42:54 -0300

    offer and search fix

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,21 +1,23 @@
 class TravelsController < ApplicationController
   def search
+    debugger
     if params
       @travels = Travel.where(
         origin: params[:origin],
         destination: params[:destination]
       )
+      @user_id = 1 #current_user.id
+      @user = User.find(@user_id)
 
       @travels = @travels.sort_by{|e| e[:origin]}
       @travels = Kaminari.paginate_array(@travels).page(params[:page]).per(5)
-
-      @user_id = 1 #current_user.id
-      @user = User.find(@user_id)
+      @signed_travels = User.find(@user_id).travels.pluck(:id)
 
       calculate_age(@user.birth_date)
       get_img_url(@user.facebook_image_url)
 
-      @signed_travels = User.find(@user_id).travels.pluck(:id) # Temporal
+      render json: TravelSearchPresenter.new(@travels,@signed_travels).to_json
+     
     end
   end
 
@@ -28,12 +30,15 @@ class TravelsController < ApplicationController
     end
     @travels = @travels.sort_by{|e| e[:origin]}
     @travels = Kaminari.paginate_array(@travels).page(params[:page]).per(5)
+
+    render json: TravelOfferedPresenter.new(@travels).to_json
   end
 
   def show
     @user_id =  1 #current_user.id
     @travel = Travel.find(params[:id]) # Temporal
     @signed_travels = User.find(@user_id).travels.pluck(:id)
+
     render json: TravelShowPresenter.new(@travel,@signed_travels).to_json
   end
 
